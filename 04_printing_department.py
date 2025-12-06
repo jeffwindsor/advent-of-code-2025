d38f00624ed5451b2b2417c7463235f6f3e0434e
Windsor, Jeff <jwindsor@cj.com>
2025-12-05 22:37:59 -0800

    Day 4 Part 2

--- 04_printing_department.py
+++ 04_printing_department.py
@@ -3,6 +3,7 @@ from aoc import (
     grid_coords,
     grid_get,
     grid_max_bounds,
+    grid_set,
     read_data_as_char_grid,
     run,
     TestCase,
@@ -16,20 +17,32 @@ def parse(data_file):
 def count_adjacent_rolls(grid, position):
     max_bounds = grid_max_bounds(grid)
     neighbors = position.neighbors(max_bounds, directions=Coord.DIRECTIONS_ALL)
-    return sum(1 for neighbor in neighbors if grid_get(grid, neighbor) == '@')
+    return sum(1 for neighbor in neighbors if grid_get(grid, neighbor) == "@")
+
+
+def is_removable(grid, position):
+    return grid_get(grid, position) == "@" and count_adjacent_rolls(grid, position) < 4
+
+
+def find_removable_rolls(grid):
+    return {pos for pos, _ in grid_coords(grid) if is_removable(grid, pos)}
 
 
 def count_accessible_rolls(data_file):
     grid = parse(data_file)
-    accessible = 0
+    return len(find_removable_rolls(grid))
 
-    for position, value in grid_coords(grid):
-        if value == '@':
-            adjacent_rolls = count_adjacent_rolls(grid, position)
-            if adjacent_rolls < 4:
-                accessible += 1
 
-    return accessible
+def count_total_removable_rolls(data_file):
+    grid = parse(data_file)
+    total_removed = 0
+
+    while removable := find_removable_rolls(grid):
+        for pos in removable:
+            grid_set(grid, pos, ".")
+        total_removed += len(removable)
+
+    return total_removed
 
 
 if __name__ == "__main__":
@@ -39,3 +52,4 @@ if __name__ == "__main__":
     ]
 
     run(count_accessible_rolls, TESTS, part="part1")
+    run(count_total_removable_rolls, TESTS, part="part2")
